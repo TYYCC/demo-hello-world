bbb241c74ed17b6605656348ffeadaed6209049d
tidycraze <2595256284@qq.com>
2026-01-27 11:01:16 +0800

    修改PA的使能方式

--- main/main.cpp
+++ main/main.cpp
@@ -131,6 +131,7 @@ static void system_init_task(void* pvParameters)
 extern "C" void app_main(void) {
     // Initialize NVS first
     esp_err_t ret = nvs_flash_init();
+    ESP_LOGI("NVS", "init = %s", esp_err_to_name(ret));
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
         ESP_ERROR_CHECK(nvs_flash_erase());
         ret = nvs_flash_init();
